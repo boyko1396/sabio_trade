45c4cda9d5d5a4a69c9eb9aaffa1dd23c443147c
Vasya <boyko1396@gmail.com>
2023-09-07 17:15:26 +0300

    asasffhwqew

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -3,6 +3,7 @@ import { filePaths } from '../config/paths.js';
 
 const copy = () => {
   return gulp.src(filePaths.src.static).pipe(gulp.dest(filePaths.build.static));
+  return gulp.src(filePaths.src.de).pipe(gulp.dest(filePaths.build.de));
 };
 
 export { copy };
